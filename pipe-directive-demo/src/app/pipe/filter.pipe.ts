95a08a9b3226ab089f6631331f0a3fc0350d5b0f
Tamás Takács <tamas.takacs91@gmail.com>
2019-08-06 11:21:06 +0200

    pipe

--- pipe-directive-demo/src/app/pipe/filter.pipe.ts
+++ pipe-directive-demo/src/app/pipe/filter.pipe.ts
@@ -6,9 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
 
   transform(value: any, key: string, phrase: string): any {
-    return value.filter( item => {
+    return value.filter(item => {
       return item[key].toLowerCase().indexOf(phrase.toLowerCase()) > -1;
     });
   }
-
 }
+
+//ha az item[key] és a phrase elé még megadjuk a .toString() metódust, akkor mindent előbb
+//át is alakít stringgé, így számok esetén is működik
